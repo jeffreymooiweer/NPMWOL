a91b1b2fb82bb84008d1e55ff154791448d68b8d
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-23 00:26:55 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ip = db.Column(db.String(15), nullable=False)
+    mac = db.Column(db.String(17), nullable=False)
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
