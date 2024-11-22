291821282f687093c6087d722cd960f4907e0e0f
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-23 00:09:04 +0100

    Update DeviceList.js

--- frontend/src/components/DeviceList.js
+++ frontend/src/components/DeviceList.js
@@ -1,1 +1,18 @@
+import React from "react";
+
+function DeviceList({ devices, setSelectedDevice, deleteDevice }) {
+    return (
+        <ul>
+            {devices.map((device) => (
+                <li key={device.id}>
+                    {device.name} - {device.ip} - {device.mac}
+                    <button onClick={() => setSelectedDevice(device)}>Bewerken</button>
+                    <button onClick={() => deleteDevice(device.id)}>Verwijderen</button>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default DeviceList;
 
