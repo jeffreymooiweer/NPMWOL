97ffb4063f47262cc8dbb0b8e93d7653d4d310a1
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-23 11:32:51 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from models import init_db
 from routes import init_routes
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="build", static_url_path="")
 
 # Configureer de database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///devices.db'
@@ -15,10 +15,10 @@ with app.app_context():
 # Initialiseer de routes
 init_routes(app)
 
-# Voeg een standaardpagina toe
+# Serve frontend
 @app.route("/")
-def index():
-    return "NPMWOL Backend is running! Access the API at /api"
+def serve_frontend():
+    return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
