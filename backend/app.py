b4fc396ad382659561c1de6995d57fe72ddcc731
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-23 10:55:50 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from models import init_db
 from routes import init_routes
 
 app = Flask(__name__)
 
-# Configureer applicatie
+# Configureer de database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///devices.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialiseer routes
+# Initialiseer de database
+with app.app_context():
+    init_db(app)
+
+# Initialiseer de routes
 init_routes(app)
 
 if __name__ == "__main__":
