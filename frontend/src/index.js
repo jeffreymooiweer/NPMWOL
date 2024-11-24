ee346831900fe761c0e44dd77010e589e0d5838c
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-24 14:16:50 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
+// frontend/src/index.js
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import './styles.css'; // Voeg dit toe voor globale styles
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
