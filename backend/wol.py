f6b2a8908c84341a1e0345214b898f825e3af9e8
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-24 14:15:44 +0100

    Update wol.py

--- backend/wol.py
+++ backend/wol.py
@@ -1,10 +1,6 @@
+# backend/wol.py
+
 from wakeonlan import send_magic_packet
 
-def wake_device(mac_address):
-    """Stuur een magic packet naar het opgegeven MAC-adres."""
-    try:
-        send_magic_packet(mac_address)
-        return True
-    except Exception as e:
-        print(f"Error sending magic packet: {e}")
-        return False
+def send_wol_packet(mac_address):
+    send_magic_packet(mac_address)
