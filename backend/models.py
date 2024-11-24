871fc64b24a859541a3d472578acb54c29766c05
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-24 09:58:06 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,12 +4,12 @@ db = SQLAlchemy()
 
 class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    domain = db.Column(db.String(100), nullable=False)
     ip = db.Column(db.String(15), nullable=False)
     mac = db.Column(db.String(17), nullable=False)
 
 def init_db(app):
-    """Initialiseer de database en maak tabellen als ze niet bestaan."""
+    """Initialiseer de database."""
     db.init_app(app)
     with app.app_context():
         db.create_all()
